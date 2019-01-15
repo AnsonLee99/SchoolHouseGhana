aa08b294e4fac7692d2fbb18f5cf35b5958ffe75
AnsonLee99 <36427506+AnsonLee99@users.noreply.github.com>
2019-01-15 03:07:05 -0800

    Update crawler.sh

--- crawler.sh
+++ crawler.sh
@@ -1,1 +1,3 @@
 wget -E -H -k -p https://en.wikipedia.org/wiki/Advanced_Scientific_Data_Format
+
+# shelljs npm or child_process module (https://tutorialedge.net/javascript/nodejs/executing-shell-scripts-with-nodejs/)
