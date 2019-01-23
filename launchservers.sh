89f8a2450facb7ccf3b2d4b02574459494e7054c
ansonlee99 <all197@ucsd.edu>
2019-01-23 02:17:16 +0000

    initial commit

---
+++ launchservers.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+kalite start 
+~/Downloads/kiwix/bin/kiwix-serve --port=8000 ~/Downloads/kiwix/ZIM/phet.zim
+
+~/Downloads/kiwix/bin/kiwix-serve --library '/home/admin/Downloads/kiwix/ZIM/TED/library/ted_en_science_2016-06.zim.xml'
+
+
